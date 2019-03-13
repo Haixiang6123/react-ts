0e346d908b296e48929cf92ff5ee490908121cf2
Haixiang <haixiang6123@gmail.com>
2019-03-13 13:31:19 -0700

    Add onClick event handler

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React, {Component} from 'react';
 import Button from './Button'
 import './App.css';
 
+const fn: React.MouseEventHandler = function (event) {
+    console.log(event);
+    console.log('fn');
+}
+
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Button size="small">
+                <Button onClick={fn} size="small">
                     <span>YES</span>
                     <span>NO</span>
                 </Button>
--- src/Button.tsx
+++ src/Button.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 interface IProps {
     size?: string,
+    onClick?: React.MouseEventHandler
 }
 
 const Button: React.FunctionComponent<IProps> = function(props) {
     return (
-        <div className={`button ${props.size}`}>
+        <div className={`button ${props.size}`} onClick={props.onClick}>
             {props.children}
         </div>
     )
